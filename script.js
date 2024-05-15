046a590a25933fb4ebb880010c1c58e2854644a9
bhoszn <95551837+bhoszn@users.noreply.github.com>
2024-05-15 15:26:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// This is just a placeholder script file
+// You can add JavaScript code here to enhance your website's functionality
+
+// Example: Display a message when the payment button is clicked
+document.querySelector('.payment-button').addEventListener('click', function() {
+    alert('Redirecting to payment page...');
+    // You can add code here to redirect the user to the payment page
+});
 
